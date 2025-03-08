6562411fa7c3d7076ba3dd53791c136963dadb8a
DaoChaShao <27882272@qq.com>
2025-03-08 22:52:36 +1300

    Initial Commit

---
+++ Eater/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eater
+//
+//  Created by 刀子叉子勺 on 2025/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eater/EaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaterApp.swift
+//  Eater
+//
+//  Created by 刀子叉子勺 on 2025/3/8.
+//
+
+import SwiftUI
+
+@main
+struct EaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
